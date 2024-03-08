721d55ef17d239d76fa8f742814ff9971348b377
Julia Bryła <s25567@pjwstk.edu.pl>
2024-03-08 13:22:05 +0100

    added

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,3 +1,15 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 Console.WriteLine("Hello, World!!!!dassa!");
+
+static double setavg(int[] arr)
+{
+    int sum = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        sum += arr[i];
+    }
+
+    return sum / arr.Length;
+}
+
