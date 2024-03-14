86ace3be709604f6cba057b6dfca69afd6a56ff8
Julia <urticaxdioica@gmail.com>
2024-03-14 23:11:05 +0100

    Opis zmian w tym zatwierdzeniu

---
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            
+            static double setavggggg(int[] arr)
+            {
+                int sum = 0;
+                for (int i = 0; i < arr.Length; i++)
+                {
+                    sum += arr[i];
+                }
+
+                return sum / arr.Length;
+            }
+
+            static int findMax(int[] arr2)
+            {
+                return findMax(arr2);
+            }
+        }
+    }
+}
